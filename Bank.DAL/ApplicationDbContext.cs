f6109ff8d1693c499c08e67942dda8584ff0cdb5
pollosatyi <pollosatyi@mail.ru>
2024-06-23 15:49:14 +0300

    Добавьте файлы проекта.

---
+++ Bank.DAL/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(Console.WriteLine);
+            optionsBuilder.UseSqlServer(connectionString:"");
+        }
+    }
+}
